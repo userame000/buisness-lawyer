cd9e316c773799755ca4e1a90ae762f3fb4515ff
userame000 <shestakov.webcanape@gmail.com>
2021-11-25 17:48:34 +0300

    comm

---
+++ js/burger.js
@@ -0,0 +1,8 @@
+$(function () {
+    $(".header__burger").on("click", function () {
+        $(this).fadeOut("slow", function(){
+            
+            $('.b-menu').toggleClass('--visible')
+        })
+    });
+});
