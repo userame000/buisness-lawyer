8661155cdf313729cf76e95eba20998fce9e532e
userame000 <shestakov.webcanape@gmail.com>
2021-11-25 15:42:39 +0300

    comm

---
+++ js/swiper.js
@@ -0,0 +1,71 @@
+var swiper = new Swiper(".swiper-container", {
+    loop: false,
+    navigation: {
+        nextEl: ".video-box__arrow-right",
+        prevEl: ".video-box__arrow-left",
+    },
+    mousewheelControl: false,
+    simulateTouch: false,
+    slidesPerView:3,
+    observer: true,
+    observeParents: true,
+    breakpoints:{
+        340: {
+            slidesPerView:1
+        },
+        1280: {
+            slidesPerView:2
+        },
+        1650: {
+            slidesPerView: 3
+        }
+    }
+});
+
+
+
+
+
+
+
+
+
+
+// $(function () {
+//     $(".video-box__arrow-right").on("click", function () {
+//         if (
+//             $(".b-reviews .video-box__player").last().index() >
+//             $(".b-reviews .--visible").last().index()
+//         ) {
+//             var indexLast = $(".b-reviews .--visible").last().index();
+//             var indexFirst = $(".b-reviews .--visible").first().index();
+//             $(".b-reviews .video-box__player").eq(indexFirst);
+//             setTimeout(function () {
+//                 $(".b-reviews .video-box__player")
+//                     .eq(indexFirst)
+//                     .removeClass("--visible");
+//                 $(".b-reviews .video-box__player")
+//                     .eq(indexLast + 1)
+//                     .addClass("--visible");
+//             }, 200);
+//         }
+//     });
+//     $(".video-box__arrow-left").on("click", function () {
+//         if (
+//             $(".b-reviews .video-box__player").first().index() <
+//             $(".b-reviews .--visible").first().index()
+//         ) {
+//             var indexLast = $(".b-reviews .--visible").last().index();
+//             var indexFirst = $(".b-reviews .--visible").first().index();
+//             $(".b-reviews .video-box__player").eq(indexLast);
+//             setTimeout(function () {
+//                 $(".b-reviews .video-box__player")
+//                     .eq(indexLast)
+//                     .removeClass("--visible");
+//                 $(".b-reviews .video-box__player")
+//                     .eq(indexFirst - 1)
+//                     .addClass("--visible");
+//             }, 200);
+//         }
+//     });
+// });
