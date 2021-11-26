73f41120dc34aa6ad41aab870fad37434ed522bd
userame000 <shestakov.webcanape@gmail.com>
2021-11-26 14:14:46 +0300

    comm

---
+++ js/owl.js
@@ -0,0 +1,26 @@
+$(function () {
+    $(".owl-carousel").owlCarousel({
+        nav: true,
+        navText: [
+            '<div class="video-box__arrow-left"></div>',
+            '<div class="video-box__arrow-right"></div>',
+        ],
+        items: 1,
+        loop: true,
+        video: true,
+        lazyLoad: true,
+        center: true,
+        marge: true,
+        responsive: {
+            340: {
+                items: 1,
+            },
+            1280: {
+                items: 1,
+            },
+            1650: {
+                items: 3,
+            },
+        },
+    });
+});
