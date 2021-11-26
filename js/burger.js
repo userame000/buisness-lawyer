581130086a894f9252186f5e2a3a5bbf39dbed47
userame000 <shestakov.webcanape@gmail.com>
2021-11-26 16:58:51 +0300

    comm

--- js/burger.js
+++ js/burger.js
@@ -1,7 +1,6 @@
 $(function () {
     $(".header__burger").on("click", function () {
         $(this).fadeOut("slow", function(){
-            
             $('.b-menu').toggleClass('--visible')
         })
     });
